98552b6ce20f0f701c3257dfae7c76240ca43c37
Jim Watkins <james.watkins77@yahoo.com>
2016-06-30 10:52:27 -0600

    finished plagiarism checker

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ var sources = [
    "quote": 'There is nothing that scares me more than, like, being in the ocean by myself.'
   }
 ]
+
+$("#trigger").click(function() {
+  var input = ($("#user_input").val());
+  includes(input);
+});
+
+function includes(string){
+ $('#notice').empty();
+ $(sources).each(function(i, obj){
+   if(string.includes(obj.quote)){
+     $('#notice').append(`<h4>You have stolen this from ${obj.author}.</h4>`)
+   } else {
+     return $('#notice').html(`<h4>Congratulations! You have created original, authentic work.</h4>`)
+   }
+ })
+};
