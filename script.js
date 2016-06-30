c25c80c7666d16598af5fb5a87065cca3c81206b
Sarah Healey <s.deane.healey@gmail.com>
2016-06-30 10:58:56 -0600

    completed

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+
+
+
+
+
+
+
+
+$( document ).ready(function() {
+    $("#trigger").click(function() {
+      var userInput = $("#user_input").val().replace(/\s\s+/g, ' ');
+      for (i=0; i< sources.length; i++) {
+      if (userInput.indexOf(sources[i].quote) > -1) {
+        // var newNode = $(li);
+$("#notice").append("<br> you have stolen from " + sources[i].author);
+      }
+     }
+    });
+});
+
+
+
 var sources = [
   {"author": "Sonia Gandhi",
    "quote": 'Together we can face any challenges as deep as the ocean and as high as the sky.'
@@ -8,4 +30,4 @@ var sources = [
   {"author": "Shawn Mendes",
    "quote": 'There is nothing that scares me more than, like, being in the ocean by myself.'
   }
-]
+];
