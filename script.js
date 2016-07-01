b763921c02fdd3b99e70f94563346339a93fab28
Alex Black <alexanderblack@Alexanders-MacBook-Air-2.local>
2016-07-01 09:56:25 -0600

    Complete

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ var sources = [
    "quote": 'There is nothing that scares me more than, like, being in the ocean by myself.'
   }
 ]
+
+$("document").ready(function(){
+  $("#trigger").on("click", function(){
+        var userInput =  $("#user_input").val().replace(/\s/g,"");
+        var gandhiName = sources[0].author;
+        var ariandaName = sources[1].author;
+        var mendesName = sources[2].author;
+        $("#notice").html("");
+        if(userInput.indexOf(sources[0].quote.replace(/\s/g,"")) != -1){
+          $("#notice").append("<p>You copied from "+gandhiName+"</p>")
+      }
+        if(userInput.indexOf(sources[1].quote.replace(/\s/g,"")) != -1){
+          $("#notice").append("<p>You copied from "+ariandaName+"</p>")
+      }
+        if(userInput.indexOf(sources[2].quote.replace(/\s/g,"")) != -1){
+          $("#notice").append("<p>You copied from "+mendesName+"</p>")
+      }
+   })
+})
