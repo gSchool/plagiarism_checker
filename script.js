58a2d06e5d265057ef4e64989583a79a3c925a2d
Rick Van <x.rckvan@gmail.com>
2016-07-01 18:15:13 -0600

    finished plagiarism

--- script.js
+++ script.js
@@ -9,41 +9,39 @@ var sources = [
    "quote": 'There is nothing that scares me more than, like, being in the ocean by myself.'
   }
 ]
-// // console.log("sources ",sources[0]);
-//
-// var btn = document.getElementById('trigger');
-// var inpt = document.getElementById('user_input').value;
-// var notice = document.getElementById('notice')
-// var val = $.trim($("textarea").val());
-// // console.log("input: ", inpt);
-// String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
-//
-//
-// for (var i = 0; i < sources.length; i++) {
-// var str = sources[i].quote
-// var auth = sources[i].author
-// var n = val.includes(str);
-// console.log("n ",n);
-// if(n===false){
-//   console.log("NOPE");
-// }else if(n===true){
-//   btn.addEventListener("click",callback);
-//   function callback(){
-//     notice.innerHTML = ""
-//     var add = document.createElement("p")
-//     add.innerHTML = "You have stolen from "+auth
-//     notice.appendChild(add);
-// }
-// }
-// }
-// in class solution***********************
-$("#trigger").click(function(){
-  $("#notice").html("");
-  sources.forEach(function(source){
-    //finds the value of input and .replace or ignore white spaces and tabs
-    var inpt = $("#user_input").val().replace(/\s\s+/g, ' ');
-    if (inpt.indexOf(source.quote)>-1) {
-      $("#notice").append("you've stolen from "+source.author)
+
+var btn = document.getElementById('trigger');
+var inpt = document.getElementById('user_input');
+var notice = document.getElementById('notice')
+var val = $.trim($("textarea").val());
+
+String.prototype.contains = function(it) { return this.indexOf(it) != -1; };
+
+
+btn.addEventListener("click",callback);
+function callback(){
+  notice.innerHTML = ""
+  for (var i = 0; i < sources.length; i++) {
+    var str = sources[i].quote
+    var auth = sources[i].author
+    var n = inpt.value.replace(/\s\s+/g, ' ')
+    // console.log("n ",n);
+    if(n.indexOf(str)>-1){
+      var add = document.createElement("p")
+      // console.log("NOPE");
+      add.innerHTML = "You have stolen from "+auth
+      notice.appendChild(add);
     }
-  })
-})
+}
+}
+// in class solution***********************
+// $("#trigger").click(function(){
+//   $("#notice").html("");
+//   sources.forEach(function(source){
+//     //finds the value of input and .replace or ignore white spaces and tabs
+//     var inpt = $("#user_input").val().replace(/\s\s+/g, ' ');
+//     if (inpt.indexOf(source.quote)>-1) {
+//       $("#notice").append("you've stolen from "+source.author)
+//     }
+//   })
+// })
